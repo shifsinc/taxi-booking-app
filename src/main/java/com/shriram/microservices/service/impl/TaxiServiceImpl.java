15e5554b334378f416dc0aefc4bdaf67f64568cc
T.S.Shriram <ts_shriram@mckinsey.com>
2016-10-13 18:01:28 +0530

    Implementing Trip Service methods;

--- src/main/java/com/shriram/microservices/service/impl/TaxiServiceImpl.java
+++ src/main/java/com/shriram/microservices/service/impl/TaxiServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
--- src/main/java/com/shriram/microservices/service/impl/TripServiceImpl.java
+++ src/main/java/com/shriram/microservices/service/impl/TripServiceImpl.java
@@ -4,10 +4,12 @@ import com.shriram.microservices.model.customer.Customer;
 import com.shriram.microservices.model.location.Location;
 import com.shriram.microservices.model.taxi.Taxi;
 import com.shriram.microservices.model.trip.Trip;
+import com.shriram.microservices.service.BillingService;
 import com.shriram.microservices.service.TaxiService;
 import com.shriram.microservices.service.TripService;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -20,14 +22,23 @@ public class TripServiceImpl implements TripService {
 
     Logger logger = Logger.getLogger(TripServiceImpl.class);
 
+    @Autowired
+    @Qualifier("billingService")
+    private BillingService billingService;
+
     @Override
     public Trip startTrip(Customer customer, Taxi taxi, Location startLocation, String startTime) {
-        return null;
+
+        Trip trip = new Trip(customer, taxi, startLocation, startTime);
+        return trip;
     }
 
     @Override
     public Trip endTrip(Trip trip) {
-        return null;
+
+        //Retrieving the bill and setting to trip
+        trip.bill(billingService.retrieveBill(trip));
+        return trip;
     }
 
 }
