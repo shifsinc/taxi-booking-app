50149c813e85dd6444b16781dac56c01398d3376
T.S.Shriram <ts_shriram@mckinsey.com>
2016-10-13 19:14:00 +0530

    Hardcoding the trip instead of retrieving from DB;

--- src/main/java/com/shriram/microservices/service/impl/TripServiceImpl.java
+++ src/main/java/com/shriram/microservices/service/impl/TripServiceImpl.java
@@ -7,6 +7,7 @@ import com.shriram.microservices.model.trip.Trip;
 import com.shriram.microservices.service.BillingService;
 import com.shriram.microservices.service.TaxiService;
 import com.shriram.microservices.service.TripService;
+import com.shriram.microservices.util.DateTimeUtil;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 @Qualifier(value = "tripService")
@@ -26,6 +28,12 @@ public class TripServiceImpl implements TripService {
     @Qualifier("billingService")
     private BillingService billingService;
 
+    @Autowired
+    @Qualifier("domainProperties")
+    private Properties domainProperties;
+
+    private final static String TIMESTAMP_FORMAT = "datetime.token.format";
+
     @Override
     public Trip startTrip(Customer customer, Taxi taxi, Location startLocation, String startTime) {
 
@@ -35,6 +43,20 @@ public class TripServiceImpl implements TripService {
 
     @Override
     public Trip endTrip(Trip trip) {
+
+        //TODO - Retrieve from DB
+        Location location = new Location(Double.parseDouble("23.2323434"), Double.parseDouble("24.232323"));
+
+        Taxi taxi = new Taxi("1");
+        taxi.location(location);
+        taxi.inTransit(true);
+
+        trip.customer(new Customer("1"));
+        trip.taxi(taxi);
+        trip.startLocation(location);
+        trip.startTime("10/13/2016 19:00:00");
+        trip.endTime("10/13/2016 19:15:00");
+
 
         //Retrieving the bill and setting to trip
         trip.bill(billingService.retrieveBill(trip));
