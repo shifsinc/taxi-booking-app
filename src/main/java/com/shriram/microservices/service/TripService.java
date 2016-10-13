e53f170c0a89285696a7c4079f51a99cdf073c9b
T.S.Shriram <ts_shriram@mckinsey.com>
2016-10-13 15:19:35 +0530

    Trip service skeleton;

---
+++ src/main/java/com/shriram/microservices/service/TripService.java
@@ -0,0 +1,31 @@
+package com.shriram.microservices.service;
+
+import com.shriram.microservices.model.customer.Customer;
+import com.shriram.microservices.model.location.Location;
+import com.shriram.microservices.model.taxi.Taxi;
+import com.shriram.microservices.model.trip.Trip;
+
+import java.util.List;
+
+public interface TripService {
+
+    /**
+     * Starts a trip
+     * <p>
+     * This method starts a trip for a customer and a taxi
+     *
+     * @return Trip
+     */
+    Trip startTrip(Customer customer, Taxi taxi);
+
+    /**
+     * Ends a trip
+     * <p>
+     * This method ends a trip and returns the bill as a part of the trip
+     *
+     * @param trip -The trip to be stopped
+     * @return Trip with billing details
+     */
+    Trip endTrip(Trip trip);
+
+}
---
+++ src/main/java/com/shriram/microservices/service/impl/TripServiceImpl.java
@@ -0,0 +1,32 @@
+package com.shriram.microservices.service.impl;
+
+import com.shriram.microservices.model.customer.Customer;
+import com.shriram.microservices.model.location.Location;
+import com.shriram.microservices.model.taxi.Taxi;
+import com.shriram.microservices.model.trip.Trip;
+import com.shriram.microservices.service.TaxiService;
+import com.shriram.microservices.service.TripService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Qualifier(value = "tripService")
+public class TripServiceImpl implements TripService {
+
+    Logger logger = Logger.getLogger(TripServiceImpl.class);
+
+    @Override
+    public Trip startTrip(Customer customer, Taxi taxi) {
+        return null;
+    }
+
+    @Override
+    public Trip endTrip(Trip trip) {
+        return null;
+    }
+
+}
