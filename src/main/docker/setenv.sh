65aa79c01729e9633b3bf4a3c13ea181c9bf421f
T.S.Shriram <ts_shriram@mckinsey.com>
2016-10-14 23:55:48 +0530

    Removing Docker configuration and plugin

--- src/main/docker/setenv.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_OPTS="$JAVA_OPTS -Dprofile="$profile
-export JAVA_OPTS="$JAVA_OPTS -Dusername="$username
-export JAVA_OPTS="$JAVA_OPTS -Dpassword="$password
-export JAVA_OPTS="$JAVA_OPTS -Ddbhostip="$dbhostip
